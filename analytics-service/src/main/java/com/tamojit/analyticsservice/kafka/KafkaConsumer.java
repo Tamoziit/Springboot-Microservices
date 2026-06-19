add33ceee92f1b974ed930492c0818eac0c0e85a
Tamojit Das <tamojitdas181007@gmail.com>
2026-06-19 21:18:24 +0530

    kafka consumers & analytics-service

---
+++ analytics-service/src/main/java/com/tamojit/analyticsservice/kafka/KafkaConsumer.java
@@ -0,0 +1,31 @@
+package com.tamojit.analyticsservice.kafka;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaConsumer {
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    // Listening on kafka topic - patient; groupId specifies the consumer id & Partitioning in a multi-consumer pipeline
+    @KafkaListener(topics = "patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event) {
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+
+            /* Any business logic related to analytics */
+
+            log.info("Received Patient Event:\nPatient Id = {}\nPatient name = {}\nPatient Email = {}",
+                patientEvent.getPatientId(),
+                patientEvent.getName(),
+                patientEvent.getEmail()
+            );
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error in Deserializing event {}", e.getMessage());
+        }
+    }
+}
